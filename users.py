60b2c68eecb693fb1e76b607e8add4fa21a0147d
Newton Rhomel Beck Angelini <newton.beck@personio.de>
2022-12-24 10:07:40 +0100

    Makes implementation detail functions private

--- users.py
+++ users.py
@@ -3,7 +3,7 @@ from os.path import exists, expanduser
 from yaml import load, dump, CLoader as Loader
 
 def introduce(alias, name, email):
-    config = read()
+    config = __read()
 
     new_user = {
         'alias': alias,
@@ -16,33 +16,32 @@ def introduce(alias, name, email):
 
     config['users'] = current_users_without_new_user
 
-    write(config)
-    
+    __write(config)
 
-def read():
-    create_config_file_when_it_does_not_exist()
-    config_file = open(get_config_file_path(), 'r').read()
+def __read():
+    __create_config_file_when_it_does_not_exist()
+    config_file = open(__get_config_file_path(), 'r').read()
     return load(config_file, Loader=Loader)
 
-def write(config):
-    with open(get_config_file_path(), 'w') as out_file:
+def __write(config):
+    with open(__get_config_file_path(), 'w') as out_file:
         dump(config, out_file, default_flow_style=False)
 
-def create_config_file_when_it_does_not_exist():
-    config_directory_path = get_config_directory_path()
+def __create_config_file_when_it_does_not_exist():
+    config_directory_path = __get_config_directory_path()
     if not exists(config_directory_path):
         mkdir(config_directory_path)
 
-    config_file_path = get_config_file_path()
+    config_file_path = __get_config_file_path()
     if not exists(config_file_path):
         base_config = {
                 'users': [],
                 'current_session': []
             }
-        write(base_config)
+        __write(base_config)
 
-def get_config_directory_path():
+def __get_config_directory_path():
     return expanduser('~') + '/.git-with'
 
-def get_config_file_path():
-    return get_config_directory_path() + '/config.yaml'
+def __get_config_file_path():
+    return __get_config_directory_path() + '/config.yaml'
